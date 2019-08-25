4d82ff8f6720fe14a0ccee2439750893c1734da8
Haroon khan <haroonkhan9426@gmail.com>
2019-08-25 15:35:53 +0500

    List of names generated succesfully

--- lib/screens/names_list_screen.dart
+++ lib/screens/names_list_screen.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class NamesListScreen extends StatelessWidget{
+  
+  final wordsList = List<WordPair>();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator')
       ),
-      body: _generateBody()
+      body: _generateBody(context)
     );
   }
 
-  Widget _generateBody(){
-    return Text('Body');
+  Widget _generateBody(BuildContext context){
+    wordsList.addAll(generateWordPairs().take(15));
+    return ListView.separated(
+      itemCount: wordsList.length,
+      padding: EdgeInsets.all(10.0),
+      itemBuilder: (context, index) => _generateListTile(wordsList[index]),
+      separatorBuilder: (context, index) => const Divider(),
+    );
+  }
+  
+  Widget _generateListTile(WordPair wordPair){
+    return ListTile(
+      title: Text(wordPair.asPascalCase,
+      style: TextStyle(
+        fontSize: 20.0,
+        color: Colors.black
+      )));
   }
 
 }
